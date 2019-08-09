1bdf5b075f1b4c944116a214abd5ad15a8040674
Lex von Biedenfeld <lexvonbon@users.noreply.github.com>
2019-08-08 22:53:59 -0500

    Remove UI components

--- src/csse.memebreaker.core/memebreaker/core/App.java
+++ src/csse.memebreaker.core/memebreaker/core/App.java
@@ -1,17 +1,8 @@
 package memebreaker.core;
 
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
-
-import memebreaker.ui.MainWindow;
-
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Module!");
-
-        SwingUtilities.invokeLater(new Runnable(){
-            public void run() { new MainWindow(); }
-        });
     }
 }
 
