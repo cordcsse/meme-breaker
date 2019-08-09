8d323947ee68e8cba855384c6aad3295a2d12391
Lex von Biedenfeld <lexvonbon@users.noreply.github.com>
2019-08-08 22:57:21 -0500

    Simplify & Clean AppConfig

--- src/csse.memebreaker.core/memebreaker/core/AppConfig.java
+++ src/csse.memebreaker.core/memebreaker/core/AppConfig.java
@@ -1,35 +1,30 @@
 package memebreaker.core;
-/**
- * AppConfiguration Class (Optional: Singleton)
- */
-import java.lang.reflect.Array;
-import java.util.*;
-import java.util.Map.Entry;
 
+import java.util.*;
 /**
- * Configuration Class </p>
+ * AppConfig<E> Class (optionally: Singleton)</p>
  * 
  * This class will take a List<Options> (Options -> { name, default, value })
- *   inserts them into a <Set>  </p>
+ *   inserts them into a <Map>  </p>
  * 
  * input: List<AllowedOptions>
  * output: Value<Option> | List<AllowedOptions> | List<SetOptions> | SetValue(<Value>)
- */
-public final class AppConfig<E extends Enum> {
-    private static class OptionValue {
+ */ 
+public final class AppConfig<E extends Enum<E>> {
+    private final static class OptionValue {
         private final String defaultValue;
         private Optional<String> value;
 
-        protected OptionValue() { this("", null); }
-        protected OptionValue(final String defaultValue) { this((defaultValue == null) ? "" : defaultValue, null); }
-        protected OptionValue(final String defaultValue, String initialValue) {
+        public OptionValue() { this("", null); }
+        public OptionValue(final String defaultValue) { this((defaultValue == null) ? "" : defaultValue, null); }
+        public OptionValue(final String defaultValue, final String initialValue) {
             this.defaultValue = defaultValue;
             this.value = Optional.ofNullable(initialValue);
         }
-        public String get() {
+        public final String get() {
             return(this.value.isPresent() ? this.value.get() : this.defaultValue);
         }
-        public void set(String value) {
+        public final void set(final String value) {
             this.value = Optional.ofNullable(value);
         }
     }
@@ -39,27 +34,28 @@ public final class AppConfig<E extends Enum> {
 
     public AppConfig(final Class<E> c) {
         this.allowedOptions = c;
-        for (E option : allowedOptions.getEnumConstants()) {
+        this.options = new EnumMap<>(c);
+        for (final E option : allowedOptions.getEnumConstants()) {
             options.putIfAbsent(option, new OptionValue());
         }
     }
-    public AppConfig(final Class<E> c, E optionKey, String optionValue) {
+    public AppConfig(final Class<E> c, final E optionKey, final String optionValue) {
         this(c);
         this.setDefault(optionKey, optionValue);
     }
-    public AppConfig(final Class<E> c, Map<E, String> optionMap) {
+    public AppConfig(final Class<E> c, final Map<E, String> optionMap) {
         this(c);
         optionMap.entrySet().stream()
             .forEach(e -> this.setDefault(e.getKey(), e.getValue()));
     }
-    private void setDefault(E optionKey, String optionValue) {
+    private void setDefault(final E optionKey, final String optionValue) {
         options.put(optionKey, new OptionValue(optionValue));
     }
 
-    public String getOption(E optionKey) {
+    public String getOption(final E optionKey) {
         return(this.options.get(optionKey).get());
     }
-    public void setOption(E optionKey, String optionValue) {
+    public void setOption(final E optionKey, final String optionValue) {
         this.options.get(optionKey).set(optionValue);
     }
 }
