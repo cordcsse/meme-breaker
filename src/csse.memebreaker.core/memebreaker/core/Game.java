8b82d1413b37a25746872029a680def9aadf2a83
Lex von Biedenfeld <lexvonbon@users.noreply.github.com>
2019-08-13 14:32:17 -0500

    Frame core thread methods

--- src/csse.memebreaker.core/memebreaker/core/Game.java
+++ src/csse.memebreaker.core/memebreaker/core/Game.java
@@ -1,8 +1,25 @@
 package memebreaker.core;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello Module!");
+/**
+ * The Runnable Game Class </p>
+ * 
+ * This class is the working logic for the MemeBreaker Game.  It is expected to run
+ *   in a background thread for responsiveness and concurrency.
+ */
+public class Game implements Runnable {
+    private Boolean quit = false;
+
+    public synchronized void stop() {
+        this.quit = true;
+    }
+    public synchronized void pause() {
+        
+    }
+
+    public void run() {
+        while (quit == false) {
+            System.out.println("Hello Module!");
+        }
     }
 }
 
